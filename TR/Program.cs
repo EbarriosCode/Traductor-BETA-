b9964092a2b13ff430547ee1e482611f58744856
Eduardo Barrios <guayoswing@gmail.com>
2017-02-14 12:54:44 -0600

    Subiendo archivos Base del traductor en C#

---
+++ TR/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TR.traducir;
+
+namespace TR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool salir = false;
+            String palabra;
+            String seguir;
+
+            while (!salir)
+            {
+                Console.WriteLine("Introduce una palabra para traducir a Kakchiquel: ");
+                palabra = Console.ReadLine();
+
+                traduction(palabra);
+
+                Console.WriteLine("\nSeguir traduciendo???  s/n");
+                seguir = Console.ReadLine();
+                salir = seguir.Equals("s") ? false : true;
+            }
+
+            Console.WriteLine("Presione cualquier tecla para salir");
+            Console.ReadKey();
+        }
+
+        public static void traduction(string palabra)
+        {
+            Traductorio traduce = new Traductorio();
+            string msjError = traduce.noEncontrado.ToString();
+
+            traduce.Entrada = palabra;
+            var respuesta = traduce.Traducir();
+
+            //Console.WriteLine(respuesta);
+            //if (respuesta.Equals(msjError)) { 
+                Console.WriteLine($"{palabra} traducido a Kakchikel es: {respuesta}");
+            //}
+            //else { 
+            //    Console.WriteLine("Palabra no encontrada");
+            //}
+
+        }
+    }
+}
---
+++ TR/traducir/Traductorio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR.traducir
+{
+    public class Traductorio
+    {
+        private string entrada;
+        public string[] kakchikel = { "jolomaj","xikinaj","tzamaj","eyaj","telemaj","tz´umaj","tz´ikaj","ruwi´ q´ab´aj","pamaj","pamaj" };
+        public string[] español = { "cabeza", "oreja", "nariz", "diente", "hombro", "pecho", "codo","dedos","estómago","estomago"};
+        public string[] noEncontrado = { "Palabra no encontrada :("};
+
+        public string Entrada
+        {
+            get { return entrada; }
+            set { entrada = value; }
+        }
+
+        private string salida;
+
+        public string Salida
+        {
+            get { return salida; }
+            set { salida = value; }
+        }
+
+        public string Traducir()
+        {
+            bool encontrar = false;
+            int contador = 0;
+
+            while (encontrar == false)
+            {
+                if (Entrada.Equals(español[contador]))
+                {
+                    salida = kakchikel[contador];
+                    encontrar = true;
+                }
+
+                //if (!Entrada.Equals(español[contador]))
+                //{
+                //    salida = noEncontrado[contador];
+                //    encontrar = true;
+                //}
+                contador++;
+            }
+            return salida;
+        }
+    }
+}
